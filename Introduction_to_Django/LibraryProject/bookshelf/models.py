188e9361d577d43a5d7e1e7a62683345777071f3
Brasen Ethan Kwame Dawson <139929813+brasendawson@users.noreply.github.com>
2024-11-03 22:50:06 +0000

    Update models.py

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
-    published_date = models.DateField()
+    publication_year = models.IntegerField()  # Ensure this is defined as a field
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
 
 # Create your models here.
