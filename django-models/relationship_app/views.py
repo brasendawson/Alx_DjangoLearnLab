923ec173070cd54a421c85c84047a43eb9244ce9
Brasen Ethan Kwame Dawson <139929813+brasendawson@users.noreply.github.com>
2024-11-10 22:03:25 +0000

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Book
 from django.views.generic.detail import DetailView
+from .views import list_books
 
 # Function-Based View to list all books
 def list_books(request):
