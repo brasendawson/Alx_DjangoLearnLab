3edd04198d6e7d62005ae19fd789f3032ca2d4c2
Brasen Ethan Kwame Dawson <139929813+brasendawson@users.noreply.github.com>
2024-11-10 21:33:58 +0000

    alx

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -4,13 +4,13 @@ class Author(models.Model):
     name = models.CharField(max_length=100)
 class Book(models.Model):
     title = models.CharField(max_length=100)
-    author = models.ForeignKey(Author)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
 class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book)
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
 
 
 # Create your models here.
