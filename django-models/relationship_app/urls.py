1bc5129a8a9312a42a82cb3512b117bba9364ef3
Brasen Ethan Kwame Dawson <139929813+brasendawson@users.noreply.github.com>
2024-11-10 22:15:54 +0000

    Update urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -9,3 +9,9 @@ urlpatterns = [
     # URL pattern for the class-based view to show library details
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 ]
+
+urlpatterns = [
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('register/', views.UserRegisterView.as_view(), name='register'),
+]
