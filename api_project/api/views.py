ed3f93d8ff48d2a32b364e5eb18f74baa4e2721c
Brasen Ethan Kwame Dawson <139929813+brasendawson@users.noreply.github.com>
2024-11-24 22:33:23 +0000

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -8,8 +8,13 @@ from rest_framework.permissions import IsAuthenticated
 from .permissions import IsAuthorOrReadOnly
 
 class BookList(generics.ListAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
 
 class BookViewSet(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication]
