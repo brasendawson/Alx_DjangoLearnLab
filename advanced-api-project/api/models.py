f3a5f9dca9f0119e3fb3bd8bd75d4ad079317527
Brasen Ethan Kwame Dawson <139929813+brasendawson@users.noreply.github.com>
2024-12-08 12:16:35 +0000

    alx

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Author(models.Model): # The Author model represents a writer with a one-to-many relationship to books.
     name = models.CharField(max_length=255)
@@ -13,3 +14,6 @@ class Book(models.Model): # The Book model contains details about books, each li
 
     def __str__(self):
         return self.title
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
